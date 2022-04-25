fcdfeeb46d0ae76e451bdc822c2e4e04f39c435a
unknown <xpalovc@ad.fi.muni.cz>
2022-04-25 18:53:15 +0200

    changes

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,7 +1,6 @@
 # contains bunch of buggy examples
-# taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
-import cPickle
-import subprocess
+# taken from 
+# https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
 import base64
 import subprocess
 import flask
